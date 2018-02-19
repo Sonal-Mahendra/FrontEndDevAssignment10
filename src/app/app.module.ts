f723a4a6e0297ac638a86901cdde380cb276dc3f
Sonal <mahendra.sonal@gmail.com>
2018-02-19 11:42:36 +0530

    FrontEndAssignment10
    Assignment10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import{ ReactiveFormsModule,FormsModule} from '@angular/forms';
+import{ReactiveFormComponent} from './reactive-form/reactive-form.component'
 import { AppComponent } from './app.component';
+import {MyCodeServiceService} from './my-code-service.service',
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    ReactiveFormsModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [MyCodeServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
